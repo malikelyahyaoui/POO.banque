e30ec39cb29553f8d4ce827271c7fa895f70f15a
malikelyahyaoui <elyahyaoui.malik67@gmail.com>
2024-10-22 16:56:09 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,12 +3,29 @@
 require 'Titulaire.php';
 require 'CompteBancaire.php';
 
-$t1 = new Titulaire('Pierre', 'jean', '1990-05-15', 'strasbourg');
-$t2 = new Titulaire('Said', 'jesus', '1985-10-20', 'mulhouse');
+$t1 = new Titulaire('Abdelmalik', 'El yahyaoui', '2007-08-22', 'STRASBOURG');
+$t2 = new Titulaire('Yassine', 'El yahyaoui', '2000-01-20', 'MULHOUSE');
 
 $c1 = new CompteBancaire('Compte Courant', 5000, 'EUR', $t1);
 $c2 = new CompteBancaire('Livret A', 10000, 'EUR', $t1);
 $c3 = new CompteBancaire('Compte Courant', 7500, 'EUR', $t2);
 
- echo $t1->afficherInformations();
+echo $t1->afficherInformations();
 echo $t2->afficherInformations();
+
+echo "{$c1} avant crédit : Solde initial {$c1->getSolde()} {$c1->getDevise()}<br>";
+$c1->crediter(2000);
+echo "{$c1} après crédit : Solde initial {$c1->getSolde()} {$c1->getDevise()}<br>";
+echo "{$c1} avant débit : Solde initial {$c1->getSolde()} {$c1->getDevise()}<br>";
+$c1->debiter(1000);
+echo "{$c1} après débit : Solde initial {$c1->getSolde()} {$c1->getDevise()}<br>";
+
+echo "Avant le virement :<br>";
+echo "$c1: Solde initial {$c1->getSolde()} {$c1->getDevise()}<br>";
+echo "$c2: Solde initial {$c2->getSolde()} {$c2->getDevise()}<br>";
+
+$c1->virement($c2, 1000);
+
+echo "Après le virement :<br>";
+echo "$c1: Solde initial {$c1->getSolde()} {$c1->getDevise()}<br>";
+echo "$c2: Solde initial {$c2->getSolde()} {$c2->getDevise()}<br>";
