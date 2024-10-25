81b6c196a7566693091f50e95a28a1fcee812bd4
malikelyahyaoui <elyahyaoui.malik67@gmail.com>
2024-10-25 16:18:08 +0200

    Update Titulaire.php

--- Titulaire.php
+++ Titulaire.php
@@ -84,19 +84,28 @@ class Titulaire{
     public function ajouterCompte(CompteBancaire $compte){
         $this -> comptes[]= $compte;
         }
-
+//afficher info du titulaire
     public function afficherInformations(): string {
-            $age = (new DateTime())->diff($this->dateNaissance)->y;
-            $infos = "Informations du titulaire :<br>";
-            $infos .= "Nom : {$this->nom}<br>";
-            $infos .= "Prénom : {$this->prenom}<br>";
-            $infos .= "Date de naissance : {$this->dateNaissance->format('d/m/Y')} (âge : {$age} ans)<br>";
-            $infos .= "Ville : {$this->ville}<br>";
-            $infos .= "Comptes :<br>";
-            foreach ($this->comptes as $compte) {
-                $infos .= "- {$compte}<br>";
-            }
-            return $infos;
+
+                $age = (new DateTime())->diff($this->dateNaissance)->y;
+
+                $infos = "Informations du titulaire :<br>";
+
+                $infos .= "Nom : {$this->nom}<br>";
+
+                $infos .= "Prénom : {$this->prenom}<br>";
+
+                $infos .= "Date de naissance : {$this->dateNaissance->format('d/m/Y')} (âge : {$age} ans)<br>";
+
+                $infos .= "Ville : {$this->ville}<br>";
+
+                $infos .= "Comptes :<br>";
+
+                foreach ($this->comptes as $compte) {
+                    
+                    $infos .= "- {$compte}<br>";
+                }
+                return $infos;
         }
 
  }
